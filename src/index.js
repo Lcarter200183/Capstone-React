8e00588f559d7ccf83d772dd9f0d764f086b6ceb
Lydia <Lcarter200183@yahoo.com>
2025-01-15 17:27:05 -0600

    add files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import { BrowserRouter, Routes } from "react-router";
+import { Route } from "react-router";
+
+
+
 
 const root = createRoot(window.bodyTag);
- root.render(<main></main>);
+ root.render(
+ <BrowserRouter>
+ <Routes> 
+   <Route path="/" element={<Home />} />  
+   <Route path="/home" element={<Home />} />  
+   <Route path="/gallery" element={<Gallery />} />
+   <Route path="/contact" element={<Contact />} />
+   <Route path="/about" element={<About />} />
+ </Routes>
+ </BrowserRouter>
+);
