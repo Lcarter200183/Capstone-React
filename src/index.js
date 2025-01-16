02c2b52c0ae47bf2435a9db0d8515cb83e0cc68c
Lydia <Lcarter200183@yahoo.com>
2025-01-15 21:13:58 -0600

    added file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import { Link } from "react-router";
+
 
 const root = createRoot(window.bodyTag);
- root.render(<main></main>);
+ root.render(
+
+ <header>
+  <nav>
+    <Link to="home">Home |</Link>
+    <Link to="gallery">Gallery |</Link>
+    <Link>Contact |</Link>
+    <Link>About </Link>
+  </nav>
+ </header>
+);
