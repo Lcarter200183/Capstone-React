0941f0c20cf7fb95582c1fbf55480e32d17a0022
Lydia <Lcarter200183@yahoo.com>
2025-01-23 15:07:35 -0600

    added file

--- src/index.js
+++ src/index.js
@@ -4,25 +4,13 @@ import { BrowserRouter } from "react-router";
 import { Routes } from "react-router";
 import { Route } from "react-router";
 import { Home } from "../Views/Home";
-<<<<<<< HEAD
-import { Gallery } from "../Views/Gallery"; 
-import { Contact } from "../Views/Contact";
-
-=======
->>>>>>> homeBrowserRouter
 
 const root = createRoot(window.bodyTag);
  root.render(
  <BrowserRouter>
   <Routes>
-<<<<<<< HEAD
      <Route path="/home" element={<Home />} />  
-     <Route path="/gallery" element={<Gallery />} />  
-     <Route path="/contact" element={<Contact />} />
-=======
-     <Route path="/" element={<Home />} />
-     <Route path="/Home" element={<Home />} />
->>>>>>> homeBrowserRouter
+     
   </Routes>
 </BrowserRouter>
  );
