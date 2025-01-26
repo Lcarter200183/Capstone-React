e0003f30451a355a1df353de3f96c4018a605dd2
Lydia <Lcarter200183@yahoo.com>
2025-01-26 16:08:45 -0600

    updated file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-export  {
+
+export default  {
     module: {
        rules: [
            {
@@ -21,47 +20,3 @@ export  {
       devtool: "source-map"
      
 }
-export default
-{
-    module: {
-        rules: [
-           {
-            test: /\.js/,
-            exclude: /\.(scss|css)/,
-            use: "babel-loader",
-         },
-         {
-            test: /\.(scss|css)/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-         },
-    ],
-=======
-export default
-{
-    module: {
-          rules: [
-        {
-            test: /\.js/,
-            exclude: /\.(scss|css)/,
-            use: "babel-loader",
-        },
-        {
-            test: /\.(scss|css)/, 
-            use: ["style-loader", "css-loader", "sass-loader"],
-        },
-      ],
->>>>>>> contactBrowserRouter
-    },
-    
-        watch: true,
-        mode: "development",
-<<<<<<< HEAD
-        devtool: "source-map",
-      
-};
->>>>>>> galleryBrowserRouter
-=======
-        devtool: "source-map"
-      
-};
->>>>>>> contactBrowserRouter
