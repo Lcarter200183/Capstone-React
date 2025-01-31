3db15e9b04a0bcc19d3a3e3c5ddda37c1dbe5cec
Lydia <Lcarter200183@yahoo.com>
2025-01-31 17:58:02 -0600

    updated file

--- controllers/handleOnClick.js
+++ controllers/handleOnClick.js
@@ -1,7 +1,7 @@
-// import React from "react";
+import React from "react";
 
-// export function handlOnClick() {
-//     return(
+export function handlOnClick() {
+    return(
         
-//     );
-// }
+    );
+}
