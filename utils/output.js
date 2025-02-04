f98a9e2be673361abe773ac3201d505ee1cd5e1b
Lydia <Lcarter200183@yahoo.com>
2025-02-04 10:34:43 -0600

    fixed file

--- utils/output.js
+++ utils/output.js
@@ -1,12 +1,9 @@
-// export function output(message) {
-//      window.outputTag.innerHTML += message;
-    
-//  }
-// export function output(
-//     message = "",
-//     outputTag = "outputTag",
-//     shouldAppend = true
-//  ) {
-//     if (shouldAppend) window[outputTag.innerHTML] += message;
-//     else window[outputTag].innerHTML = message;
-// }
+
+export function output(
+    message = "",
+    outputTag = "outputTag",
+    shouldAppend = true
+ ) {
+    if (shouldAppend) window[outputTag.innerHTML] += message;
+    else window[outputTag].innerHTML = message;
+}
