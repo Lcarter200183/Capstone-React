e43f3895b49b416ae192a78dba0c8b455ad41271
Lydia Carter <lcarter200183@yahoo.com>
2025-02-26 17:53:53 -0600

    ignored oldFile

--- dist/main.js
+++ dist/main.js
@@ -13237,18 +13237,22 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+const domain = window.location.hostname;
+let rootPath = "";
+if (domain === "lcarter200183.github.io") rootPath = "/Capstone-React";
+console.log(`The domain is ${domain}`);
 const root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_0__.createRoot)(window.bodyTag);
 root.render(/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(react_router__WEBPACK_IMPORTED_MODULE_6__.BrowserRouter, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(react_router__WEBPACK_IMPORTED_MODULE_6__.Routes, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(react_router__WEBPACK_IMPORTED_MODULE_6__.Route, {
-  path: "/",
+  path: `${rootPath}/`,
   element: /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(_Views_Home__WEBPACK_IMPORTED_MODULE_2__.Home, null)
 }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(react_router__WEBPACK_IMPORTED_MODULE_6__.Route, {
-  path: "/gallery",
+  path: `${rootPath}/gallery`,
   element: /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(_Views_Gallery__WEBPACK_IMPORTED_MODULE_3__.Gallery, null)
 }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(react_router__WEBPACK_IMPORTED_MODULE_6__.Route, {
-  path: "/contact",
+  path: `${rootPath}/contact`,
   element: /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(_Views_Contact__WEBPACK_IMPORTED_MODULE_4__.Contact, null)
 }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(react_router__WEBPACK_IMPORTED_MODULE_6__.Route, {
-  path: "/about",
+  path: `${rootPath}/about`,
   element: /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement(_Views_About__WEBPACK_IMPORTED_MODULE_5__.About, null)
 }))));
 })();
--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Contact } from "./Views/Contact";
 import { About } from "./Views/About";
 
+const domain = window.location.hostname;
+
+let rootPath = "";
+if (domain === "lcarter200183.github.io") rootPath = "/Capstone-React";
+console.log(`The domain is ${domain}`);
 const root = createRoot(window.bodyTag);
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
+      <Route path={`${rootPath}/`} element={<Home />} />
+      <Route path={`${rootPath}/gallery`} element={<Gallery />} />
+      <Route path={`${rootPath}/contact`} element={<Contact />} />
+      <Route path={`${rootPath}/about`} element={<About />} />
     </Routes>
   </BrowserRouter>
 );
