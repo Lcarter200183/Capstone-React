296f59d0bbb6b15394a7fc475ea8c82cc35e80e2
Lydia Carter <lcarter200183@yahoo.com>
2025-02-26 17:02:06 -0600

    file info

--- src/controllers/moduleExample.js
+++ src/controllers/moduleExample.js
@@ -1,7 +1,5 @@
 import { output } from "../utils/output";
 
-window.moduleExample = moduleExample();
-
 export function moduleExample() {
   output(moduleExample);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ export default {
   module: {
     rules: [
       {
+        test: /\.js/,
+        exclude: /\.(scss|css|otf|json)/,
         use: "babel-loader",
+      },
+      {
+        test: /\.(scss|css)/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|mp4)/,
+        type: "asset/resource",
       },
     ],
   },
   watch: true,
+  watchOptions: { aggregateTimeout: 1000 },
+
   mode: "development",
   devtool: "source-map",
 };
